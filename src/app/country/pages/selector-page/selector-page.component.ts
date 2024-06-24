580c6ad0d640197916c0dc5c421c99c747ca9700
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-24 17:48:20 -0500

    clase 18 video Estructura de directorios para esta aplicación y mi solucion a la tarea

---
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'selector-page',
+  templateUrl: './selector-page.component.html',
+  styleUrl: './selector-page.component.css',
+})
+export class SelectorPageComponent {}

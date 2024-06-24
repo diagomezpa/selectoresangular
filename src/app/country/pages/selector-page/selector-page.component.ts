6fc6c905178a08f519d801eb6db51703c4ef59f0
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-24 18:53:45 -0500

    clase 19 VIDEO 256 fORMULARIO REACTIVO - PRIMER SELECTOR

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'selector-page',
   templateUrl: './selector-page.component.html',
   styleUrl: './selector-page.component.css',
 })
-export class SelectorPageComponent {}
+export class SelectorPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) {}
+}
